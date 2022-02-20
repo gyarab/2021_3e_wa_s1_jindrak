c957c25a49458267927b0f10233be1d3dcfaa242
Věnčeslav Chalupář <90437977+MetrKolben@users.noreply.github.com>
2022-02-20 19:42:50 +0100

    Úkol

--- python_intro/mujweb/gaflix/models.py
+++ python_intro/mujweb/gaflix/models.py
@@ -1,7 +1,15 @@
+from cProfile import label
 from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
+    movies = models.ManyToManyField('Movie', null=True, blank=True)
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")
+
+    def __str__(self):
+        return self.title
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
@@ -10,11 +18,15 @@ class Movie(models.Model):
     director = models.ForeignKey('Director', on_delete=models.SET_NULL, null=True)
     actors = models.ManyToManyField('Actor')
     categories = models.ManyToManyField('Category')
+    def __str__(self):
+        return self.title
 
 class Person(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+    def __str__(self):
+        return self.name
 
 class Actor(Person):
     pass
