c605b91bb7a9459c7b9789d01d9dd638972d65e7
Věnčeslav Chalupář <90437977+MetrKolben@users.noreply.github.com>
2022-02-24 11:47:07 +0100

    updat

--- python_intro/mujweb/gaflix/models.py
+++ python_intro/mujweb/gaflix/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
-    movies = models.ManyToManyField('Movie', null=True, blank=True)
+    movies = models.ManyToManyField('Movie', blank=True)
     class Meta:
         verbose_name = ("Category")
         verbose_name_plural = ("Categories")
